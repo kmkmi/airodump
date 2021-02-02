1ea0f249bc2cfc713e67705242eebaacb38f5d24
kmkmi <jk787787@naver.com>
2021-02-02 10:15:02 +0900

    Update main.h

--- main.h
+++ main.h
@@ -89,11 +89,17 @@ typedef struct Rtap{
 
 
 typedef struct Frame_Control_Field{
-    uint8_t version:2;
-    uint8_t type:2;
     uint8_t subtype:4;
+    uint8_t type:2;
+    uint8_t version:2;
     uint8_t flags;
 
+    void init(uint16_t i){
+        this->subtype = i >> 12;
+        this->type = (i >> 10) & 0b11;
+        this->version = (i >> 8) & 0b11;
+        this->flags = i & 0xFF;
+    }
     bool isBeaconFrame(){
         if (this->type == MANAGEMENT_FRAME && this->subtype == BEACON_FRAME)
             return true;
